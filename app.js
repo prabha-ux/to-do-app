c3f3a09b2f2c3e34102647690ce454c3b700dfff
Prabha Ganapathi <prabha.ux@gmail.com>
2019-04-19 11:24:36 -0700

    all files updated

--- app.js
+++ app.js
@@ -1,33 +1,78 @@
 function onReady() {
+  let id = 0;
+  let toDos = localStorage.getItem("toDos");
+  console.log(toDos)
+  if(toDos == null) {
+    toDos = [];
+  } else {
+    toDos = JSON.parse(toDos);
+    console.log(toDos)
+  }
   const addToDoForm = document.getElementById('addToDoForm');
+  renderTheUI();
+
+function createNewToDo(){
   const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');//<ul></ul>
+  if (!newToDoText.value) { return; }
+  toDos.push({
+    title: newToDoText.value,
+    complete: false,
+    id: id
+  });
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+  id++;
+  newToDoText.value = '';
+  renderTheUI();
+}
 
-  addToDoForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+function renderTheUI() {
+  const toDoList = document.getElementById('toDoList');
 
-    // get the text
-    let title = newToDoText.value;
+  toDoList.textContent = '';
 
-    let newLi = document.createElement('li'); //create a new li //<li></li>
-    let checkbox = document.createElement('input'); //create a new checkbox as an input
+  toDos.forEach(function(toDo){
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
 
-    let deleteBtn = document.createElement('button'); //create delete deleteButton
-    deleteBtn.textContent = "Delete"; //adding text content to the delete deleteButton
-
-    deleteBtn.addEventListener('click', function(event){
-      //this.parentElement represents the delete button's <li> parent
-      toDoList.removeChild(this.parentElement);
+    checkbox.addEventListener('click', event => {
+      toDos.forEach (function(item){
+          if (item.id == toDo.id) {
+            if (checkbox.checked == true){
+              item.complete = true;
+            }
+            else {
+              item.complete = false;
+            }
+          }
+        }
+      )
+      localStorage.setItem("toDos", JSON.stringify(toDos));
     });
 
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-    newLi.appendChild(deleteBtn);
+    const delete_btn = document.createElement('button');
+    delete_btn.textContent = "Delete!";
+
+    delete_btn.addEventListener('click', event => {
+      toDos = toDos.filter(function(item){
+        return item.id !== toDo.id;
+      })
+      localStorage.setItem("toDos", JSON.stringify(toDos));
+      renderTheUI();
+    });
+    newLi.textContent = toDo.id + ". " + toDo.title;
+    checkbox.checked = toDo.complete;
     toDoList.appendChild(newLi);
-    newToDoText.value = "";
+    newLi.appendChild(checkbox);
+    newLi.appendChild(delete_btn);
   });
-};
+}
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+}
 
 window.onload = function() {
   onReady();
